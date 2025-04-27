e565edb0ab354138d1b84efbf01d2e2273bf187f
Raissa Nunes <raissan084@gmail.com>
2025-04-27 12:20:05 -0300

    poo

---
+++ src/main/java/poo/Pessoa.java
@@ -0,0 +1,10 @@
+package poo;
+// Novos objetos para poder fazer ações no programa com eles
+// Interação entre objetos
+
+// Classe -> objeto
+// Molde do objeto = classe
+// Cria um novo objeto = instancia
+
+public class Pessoa {
+}
