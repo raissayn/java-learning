070fbb7be3dfac5712853a44a4393f519b8d8001
Raissa Nunes <raissan084@gmail.com>
2025-05-07 15:03:49 -0300

    att estudos

--- src/main/java/fundamentosJava/poo/Pessoa.java
+++ src/main/java/fundamentosJava/poo/Pessoa.java
@@ -13,12 +13,70 @@ public class Pessoa {
     private double altura;
     private double peso;
 
-    public Pessoa(){
-        this.nome = "Sem nome";
-        this.idade = 0;
-        this.altura = 0.0;
-        this.peso = 0.0;
-    }
+//    // função construtora
+//    public Pessoa(){
+//        this.nome = "Sem nome";
+//        this.idade = 0;
+//        this.altura = 0.0;
+//        this.peso = 0.0;
+//    } // atribuindo valores iniciais
 
+
+    // função construtora com atributos
+    public Pessoa(String nome, int idade, double altura, double peso){
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
     // Métodos -> ações
+    // setters e getters
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){ //return string
+        return this.nome;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public int getIdade(){ // return int
+        return this.idade;
+    }
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
+    public double getAltura(){
+        return this.altura;
+    }
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+    public double getPeso(){
+        return this.peso;
+    }
+    public double calcularIMC(){
+        return this.peso / (this.altura * this.altura);
+    }public String verificarClassificacaoIMC(){
+        double imc = this.calcularIMC();
+        if(imc < 18.5){
+            return "Abaixo do peso";
+        }
+        if(imc >= 18.5 && imc <= 24.9){
+            return "Peso normal";
+        }
+        if(imc >= 25 && imc <= 29.9){
+            return "Sobrepeso";
+        }
+        if(imc >= 30 && imc <= 34.9){
+            return "Obesidade grau 1";
+        }
+        if(imc >= 35 && imc <= 39.9){
+            return "Obesidade grau 2";
+        }
+        return "Obesidade grau 3"; // IMC acima de 40
+    }
+    public String apresentar(){
+        return "Nome: " + this.nome + "\nIdade: " + this.idade + "\nAltura: " + this.altura + "\nPeso: " + this.peso + "IMC: "  + this.calcularIMC();
+    }
 }
--- src/main/java/fundamentosJava/poo/ProgramaPrincipal.java
+++ src/main/java/fundamentosJava/poo/ProgramaPrincipal.java
@@ -1,4 +1,32 @@
 package fundamentosJava.poo;
 
 public class ProgramaPrincipal {
+    public static void main(String[] args) {
+
+        // Criar uma pessoa => intancias (new)
+        // tipo(classe) variavel = new classe()
+        Pessoa pessoa1 = new Pessoa("Raissa", 19, 1.65, 65);
+//
+//        pessoa1.setNome("Raissa");
+//        pessoa1.setIdade(19);
+//        pessoa1.setAltura(1.65);
+//        pessoa1.setPeso(65);
+        System.out.println(pessoa1.getNome());
+        System.out.println(pessoa1.getIdade());
+        System.out.println(pessoa1.getAltura());
+        System.out.println(pessoa1.getPeso());
+        // ou
+        System.out.println(pessoa1.getNome() + " tem " + pessoa1.getIdade() + " anos" + " e tem " + pessoa1.getAltura() + " de altura" + " e pesa " + pessoa1.getPeso() + "Kg");
+
+        Pessoa pessoa2 = new Pessoa("Maria", 20, 1.70, 80); // com construtor
+        System.out.println(pessoa2.getNome());
+        System.out.println(pessoa2.getIdade());
+        System.out.println(pessoa2.getAltura());
+        System.out.println(pessoa2.getPeso());
+
+        // APRESENTANDO PELO MÉTODO
+        System.out.println(pessoa1.apresentar());
+        System.out.println(pessoa2.apresentar());
+    }
+
 }
